3c4e5b50fefc533fc1c112d09ec74d6036b7adc1
Jaydeep Suthar <technicaljade@gmail.com>
2022-05-19 22:31:00 +0530

    Comming Soon Added

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import CommingSoon from "./components/CommingSoon.vue";
+</script>
+
+<template>
+	<CommingSoon msg="Comming Soon" />
+</template>
+
+<style>
+body {
+	min-width: 400px;
+}
+
+#app {
+	font-family: "DM Sans", sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
---
+++ src/components/CommingSoon.vue
@@ -0,0 +1,29 @@
+<script setup>
+import banner from "../assets/welcome.svg";
+
+defineProps({
+	msg: String,
+});
+</script>
+
+<template>
+	<section
+		class="comming__section flex flex-col gap-5 justify-center items-center"
+	>
+		<img
+			class="comming__soon__img"
+			:src="banner"
+			alt="JaydeepSuthar.tech"
+		/>
+		<h1
+			class="comming__soon__text font-bold text-4xl lg:text-6xl hover:underline"
+		>
+			JaydeepSuthar.tech
+		</h1>
+		<p
+			class="comming__soon__text font-bold text-2xl lg:text-4xl text-slate-500"
+		>
+			{{ msg }}
+		</p>
+	</section>
+</template>
